6292d09f937fff76df01d4fba4a3fb44010d4470
Matt Young <myoung@IceCloudNP.onmicrosoft.com>
2018-06-15 13:09:59 +0100

    First pass at ui

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -4,12 +4,32 @@ import (
 	"net/http"
 	"strconv"
 	"fmt"
+	"html/template"
+	"log"
 )
 
 func Home(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/" {
 		http.NotFound(w, r)
 		return
+	}
+
+	files := []string{
+		"./ui/html/base.html",
+		"./ui/html/home.page.html",
+	}
+
+	ts, err := template.ParseFiles(files...)
+	if err!= nil{
+		log.Println(err.Error())
+		http.Error(w, "Internal Server Error", 500)
+		return
+	}
+
+	err = ts.ExecuteTemplate(w, "base", nil)
+	if err!=nil{
+		log.Println(err.Error())
+		http.Error(w, "Internal Server Error", 500)
 	}
 
 	w.Write([]byte("Hello from Snippetbox"))
