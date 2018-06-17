387dee8a88e4314be18e50439cab01a36855d03f
Matt Young <mpyoung@gmail.com>
2018-06-17 21:04:53 +0100

    Pretty

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -2,16 +2,25 @@ package main
 
 import ( "log"
 	"net/http"
+	"flag"
 )
 
 func main() {
+	addr := flag.String("addr", ":4000", "HTTP network address")
+	staticDir := flag.String("static-dir", "./ui/static", "Path to access static files")
+
+	flag.Parse()
+
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", Home)
 	mux.HandleFunc("/snippet", ShowSnippet)
 	mux.HandleFunc("/snippet/new", NewSnippet)
 
-	log.Println("Starting server on :4000")
+	fileServer := http.FileServer(http.Dir(*staticDir))
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
-	err := http.ListenAndServe(":4000", mux)
+	log.Printf("Starting server on %s", *addr)
+
+	err := http.ListenAndServe(*addr, mux)
 	log.Fatal(err)
 }
